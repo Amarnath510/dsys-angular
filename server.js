0db9dd6116b0aae486c5ca64177b53aef587afcf
Amarnath <camarnath@castlighthealth.com>
2020-12-07 23:23:00 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-// server.js
+const path = require('path');
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.use(express.static(path.join(__dirname, 'dist/dsys-angular')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + 'dist/dsys-angular/index.html'))
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
